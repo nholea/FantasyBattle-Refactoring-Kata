2523e1eb0432e8813033c6eaef885ced04886f31
Natalia <natalia.hernandez@clarity.ai>
2022-11-07 17:42:57 +0100

    refactor: delete unused methods

--- java/src/main/java/codingdojo/Player.java
+++ java/src/main/java/codingdojo/Player.java
@@ -12,7 +12,7 @@ class Player extends Target {
 
     Damage calculateDamage(Target other) {
         int baseDamage = inventory.getBaseDamage();
-        float damageModifier = inventory.getDamageModifier();
+        float damageModifier = inventory.getDamageModifier() + stats.strengthModifier();
         int totalDamage = Math.round(baseDamage * damageModifier);
         int soak = getSoak(other, totalDamage);
         return new Damage(Math.max(0, totalDamage - soak));
@@ -40,33 +40,4 @@ class Player extends Target {
         return soak;
     }
 
-//    private float getDamageModifier() {
-//        Equipment equipment = this.inventory.getEquipment();
-//        Item leftHand = equipment.getLeftHand();
-//        Item rightHand = equipment.getRightHand();
-//        Item head = equipment.getHead();
-//        Item feet = equipment.getFeet();
-//        Item chest = equipment.getChest();
-//        float strengthModifier = stats.getStrength() * 0.1f;
-//        return strengthModifier +
-//            leftHand.getDamageModifier() +
-//            rightHand.getDamageModifier() +
-//            head.getDamageModifier() +
-//            feet.getDamageModifier() +
-//            chest.getDamageModifier();
-//    }
-
-//    private int getBaseDamage() {
-//        Equipment equipment = this.inventory.getEquipment();
-//        Item leftHand = equipment.getLeftHand();
-//        Item rightHand = equipment.getRightHand();
-//        Item head = equipment.getHead();
-//        Item feet = equipment.getFeet();
-//        Item chest = equipment.getChest();
-//        return leftHand.getBaseDamage() +
-//        rightHand.getBaseDamage() +
-//        head.getBaseDamage() +
-//        feet.getBaseDamage() +
-//        chest.getBaseDamage();
-//    }
 }
