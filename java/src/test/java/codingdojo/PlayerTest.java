e827c88daf4c6c63cae1eeee2a6d4cd3693bf6ea
Natalia <natalia.hernandez@clarity.ai>
2022-11-07 16:12:03 +0100

    refactor: damageCalculationsWithMocks test

--- java/src/test/java/codingdojo/PlayerTest.java
+++ java/src/test/java/codingdojo/PlayerTest.java
@@ -16,21 +16,11 @@ public class PlayerTest {
         Stats stats = mock(Stats.class);
         SimpleEnemy target = mock(SimpleEnemy.class);
         Armor armor = mock(Armor.class);
-        Equipment equipment = mock(Equipment.class);
-        Item item = mock(Item.class);
-        Item rightHandItem = mock(Item.class);
 
-        when(inventory.getEquipment()).thenReturn(equipment);
-        when(inventory.getEquipment().getLeftHand()).thenReturn(item);
-        when(inventory.getEquipment().getRightHand()).thenReturn(rightHandItem);
-        when(inventory.getEquipment().getHead()).thenReturn(item);
-        when(inventory.getEquipment().getFeet()).thenReturn(item);
-        when(inventory.getEquipment().getChest()).thenReturn(item);
-        when(inventory.getEquipment().getRightHand().getBaseDamage()).thenReturn(10);
-        when(inventory.getEquipment().getRightHand().getDamageModifier()).thenReturn(1.0f);
+        when(inventory.getEquipment()).thenReturn(getEquipment());
         when(stats.getStrength()).thenReturn(0);
         when(armor.getDamageSoak()).thenReturn(5);
-        when(target.getArmor()).thenReturn(armor);
+        when(target.getArmor()).thenReturn(new SimpleArmor(5));
 
         Damage damage = new Player(inventory, stats).calculateDamage(target);
         assertEquals(5, damage.getAmount());
@@ -45,5 +35,16 @@ public class PlayerTest {
         SimpleEnemy target = new SimpleEnemy(null, null);
         Damage damage = new Player(inventory, stats).calculateDamage(target);
         assertEquals(10, damage.getAmount());
+    }
+
+    Equipment getEquipment(){
+        Item rigthHandItem = new BasicItem("flashy sword of danger", 10, 1.0f);
+        Item item = new BasicItem("",0,0);
+
+        return new Equipment(item,
+          rigthHandItem,
+          item,
+          item,
+          item);
     }
 }
