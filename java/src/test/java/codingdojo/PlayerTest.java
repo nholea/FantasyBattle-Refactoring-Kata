4c0dbb3c96d3d9564338670b3c2fbd2408170b83
Natalia <natalia.hernandez@clarity.ai>
2022-11-07 15:59:55 +0100

    test: create damageCalculationsWithMocks test

--- java/src/test/java/codingdojo/PlayerTest.java
+++ java/src/test/java/codingdojo/PlayerTest.java
@@ -5,19 +5,35 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class PlayerTest {
 
     // choose this one if you are familiar with mocks
-    @Disabled("Test is not finished yet")
     @Test
     void damageCalculationsWithMocks() {
         Inventory inventory = mock(Inventory.class);
         Stats stats = mock(Stats.class);
         SimpleEnemy target = mock(SimpleEnemy.class);
+        Armor armor = mock(Armor.class);
+        Equipment equipment = mock(Equipment.class);
+        Item item = mock(Item.class);
+        Item rightHandItem = mock(Item.class);
+
+        when(inventory.getEquipment()).thenReturn(equipment);
+        when(inventory.getEquipment().getLeftHand()).thenReturn(item);
+        when(inventory.getEquipment().getRightHand()).thenReturn(rightHandItem);
+        when(inventory.getEquipment().getHead()).thenReturn(item);
+        when(inventory.getEquipment().getFeet()).thenReturn(item);
+        when(inventory.getEquipment().getChest()).thenReturn(item);
+        when(inventory.getEquipment().getRightHand().getBaseDamage()).thenReturn(10);
+        when(inventory.getEquipment().getRightHand().getDamageModifier()).thenReturn(1.0f);
+        when(stats.getStrength()).thenReturn(0);
+        when(armor.getDamageSoak()).thenReturn(5);
+        when(target.getArmor()).thenReturn(armor);
 
         Damage damage = new Player(inventory, stats).calculateDamage(target);
-        assertEquals(10, damage.getAmount());
+        assertEquals(5, damage.getAmount());
     }
 
     // choose this one if you are not familiar with mocks
