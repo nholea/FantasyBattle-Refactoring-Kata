7614646ea5675006f30d3dfcab28c11c2b9174c0
Natalia <natalia.hernandez@clarity.ai>
2022-11-08 12:52:32 +0100

    refactor: remove blank lines

--- java/src/main/java/codingdojo/Inventory.java
+++ java/src/main/java/codingdojo/Inventory.java
@@ -11,14 +11,11 @@ public class Inventory {
         return equipment;
     }
 
-
     public int getBaseDamage(){
         return equipment.getAllItems().stream().mapToInt(Item::getBaseDamage).sum();
     }
 
-
     public float getDamageModifier(){return (float) equipment.getAllItems().stream().mapToDouble(Item::getDamageModifier).sum();}
-
 
     }
 
