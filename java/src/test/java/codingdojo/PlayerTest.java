6ec178b368d0bb7a02bfbffd4f52c1999f7303ce
Natalia <natalia.hernandez@clarity.ai>
2022-11-08 15:47:09 +0100

    test: implement parameterized test in PlayerTest class

--- java/src/test/java/codingdojo/PlayerTest.java
+++ java/src/test/java/codingdojo/PlayerTest.java
@@ -2,6 +2,8 @@ package codingdojo;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
@@ -9,36 +11,43 @@ import static org.mockito.Mockito.when;
 
 public class PlayerTest {
 
+    private final int DAMAGE_SOAK = 5;
+
+    private final int STRENGTH = 0;
+
     // choose this one if you are familiar with mocks
-    @Test
-    void calculatesDamageWhenPlayerIsEquippedWithFlashySwordOfDangerInHisRightHand() {
+    @ParameterizedTest
+    @CsvSource({"10, 1.0f,5", "10,2.4f,19","10, 2.5f,20", "10, 3.7f,32", "10,5.1f,46"})
+    void calculatesDamageWhenPlayerIsEquippedWithFlashySwordOfDangerInHisRightHandAndOtherItems(int baseDamage, float damageModifier, int expectedDamage) {
         Inventory inventory = mock(Inventory.class);
         Stats stats = mock(Stats.class);
         SimpleEnemy target = mock(SimpleEnemy.class);
 
-        when(inventory.getBaseDamage()).thenReturn(10);
-        when(inventory.getDamageModifier()).thenReturn(1.0f);
-        when(stats.getStrength()).thenReturn(0);
-        when(target.getSoak()).thenReturn(5);
+        when(inventory.getBaseDamage()).thenReturn(baseDamage);
+        when(inventory.getDamageModifier()).thenReturn(damageModifier);
+        when(stats.getStrength()).thenReturn(STRENGTH);
+        when(target.getSoak()).thenReturn(DAMAGE_SOAK);
 
 
         Damage damage = new Player(inventory, stats).calculateDamage(target);
-        assertEquals(5, damage.getAmount());
+        assertEquals(expectedDamage, damage.getAmount());
     }
-    @Test
-    void calculatesDamageWhenPlayerIsEquippedWithExcaliburInHisRightHand() {
+
+    @ParameterizedTest
+    @CsvSource({"20, 1.5f,25", "20,2.9f,53","20, 3.0f,55", "20, 4.2f,79", "20,5.6f,107"})
+    void calculatesDamageWhenPlayerIsEquippedWithExcaliburInHisRightHandAndOtherItems(int baseDamage, float damageModifier, int expectedDamage) {
         Inventory inventory = mock(Inventory.class);
         Stats stats = mock(Stats.class);
         SimpleEnemy target = mock(SimpleEnemy.class);
 
-        when(inventory.getBaseDamage()).thenReturn(20);
-        when(inventory.getDamageModifier()).thenReturn(1.5f);
-        when(stats.getStrength()).thenReturn(0);
-        when(target.getSoak()).thenReturn(5);
+        when(inventory.getBaseDamage()).thenReturn(baseDamage);
+        when(inventory.getDamageModifier()).thenReturn(damageModifier);
+        when(stats.getStrength()).thenReturn(STRENGTH);
+        when(target.getSoak()).thenReturn(DAMAGE_SOAK);
 
 
         Damage damage = new Player(inventory, stats).calculateDamage(target);
-        assertEquals(25, damage.getAmount());
+        assertEquals(expectedDamage, damage.getAmount());
     }
 
     // choose this one if you are not familiar with mocks
